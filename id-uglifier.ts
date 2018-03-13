ec1f7e7f000456b6a62bb65ee02e2558b2a96dee
Alexandre Bento Freire <git@a-bentofreire.com>
2018-03-13 15:03:05 +0800

    bump version

--- id-uglifier.ts
+++ id-uglifier.ts
@@ -16,7 +16,7 @@ const
   DEFAULTINPSUFFIX = '_UG',
   DEFAULTOUTPREFIX = 'ug_',
   SCRIPTNAME = 'id-uglifier',
-  VERSION = '0.2.0'; // @TIP: keep it sync with package.json version
+  VERSION = '0.2.1'; // @TIP: keep it sync with package.json version
 
 var opts = {
   isActive: true,
